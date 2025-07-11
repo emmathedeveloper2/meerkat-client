bd0ff1ec1dab62a1bce1feb6f8ddf0dbf4be6790
emmathedeveloper2 <emmadavid063@gmail.com>
2025-07-11 14:02:04 +0100

    Update

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,9 +1,9 @@
 <template>
   <main class="flex flex-col items-center justify-end p-[36px] h-dvh text-white">
-    <h1 class="font-extrabold text-2xl text-white shadow-2xl shadow-black">MEERKAT</h1>
+    <h1 class="font-extrabold text-2xl text-white drop-shadow-2xl drop-shadow-black">MEERKAT</h1>
   </main>
 </template>
-
+g
 <style scoped>
   main {
     background-image: url("/assets/welcome-page-background.png");
