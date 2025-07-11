a5bb29c02fd14db55078a401330fff8e9983f299
emmathedeveloper2 <emmadavid063@gmail.com>
2025-07-11 14:00:19 +0100

    Update

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <main class="flex flex-col items-center justify-end p-[36px] h-screen text-white">
+  <main class="flex flex-col items-center justify-end p-[36px] h-dvh text-white">
     <h1 class="font-extrabold text-2xl text-white shadow-2xl shadow-black">MEERKAT</h1>
   </main>
 </template>
