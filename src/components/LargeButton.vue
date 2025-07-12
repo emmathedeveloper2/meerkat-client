bfb8e9b3007aad8644291141743c5280b329a43d
emmathedeveloper2 <emmadavid063@gmail.com>
2025-07-12 22:28:19 +0100

    Fix

--- src/components/LargeButton.vue
+++ src/components/LargeButton.vue
@@ -5,14 +5,14 @@
 </script>
 
 <template>
-  <button class="w-max border-2 border-brown-deep bg-primary py-[10px] px-[50px] rounded-[10px] font-bold text-[20px]">
+  <button class="w-max border-2 border-brown-deep py-[10px] px-[50px] rounded-[10px] font-bold text-[20px]">
     {{ label || 'Button' }}
   </button>
 </template>
 
 <style scoped>
   button {
-    background-image: linear-gradient(ro right, #911A53 0%, #C76F0C 48% , #D0685B 100%);
+    background-image: linear-gradient(to right, #911A53 0%, #C76F0C 48% , #D0685B 100%);
     box-shadow: 0 4px 8px 5px color-mix(in srgb, var(--brown-deep) 50%, transparent);
   }
 
